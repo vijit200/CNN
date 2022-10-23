904e5ffc0dc2d6cac15f94f0d87ff28f7b9bfac8
vijit kumar <vijitkumar699@gmail.com>
2022-10-23 14:34:47 +0530

    s

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -6,6 +6,7 @@ from deepClassifier.entity import PrepareBaseModelConfig
 from deepClassifier.entity import PrepareCallbacksConfig
 import os
 from deepClassifier.entity import TrainingConfig
+from deepClassifier.entity import EvaluationConfig
 
 
 class ConfigurationManager:
@@ -89,3 +90,14 @@ class ConfigurationManager:
         )
 
         return training_config
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/PetImages",
+            mlflow_uri="https://dagshub.com/vijit200/CNN.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
---
+++ src/deepClassifier/pipeline/st_04_evaluation.py
@@ -0,0 +1,23 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import Evaluation
+from deepClassifier import logger
+
+STAGE_NAME = "Evaluation"
+
+def main():
+    config = ConfigurationManager()
+    val_config = config.get_validation_config()
+    evaluation = Evaluation(val_config)
+    evaluation.evaluation()
+    evaluation.save_score()
+    evaluation.log_into_mlflow()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
